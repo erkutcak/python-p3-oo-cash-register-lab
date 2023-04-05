c0d7f4eb2275fa64a6fc6cea37e90df6731c8f83
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-05 15:01:56 +0300

    lab complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+# CashRegister in cash_register.py subtracts the last item from the total F                                                                        [ 92%]
+# CashRegister in cash_register.py returns the total to 0.0 if all items have been removed F 
+
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0
+    self.prev_total = 0
+    self.items = []
+  
+  def add_item(self, title, price, quantity = 1):
+    self.prev_total = price * quantity
+    self.total += self.prev_total
+    for _ in range(quantity):
+      self.items.append(title)
+  
+  def apply_discount(self):
+    if (self.discount > 0):
+      self.total -= int(self.total * self.discount / 100)
+      print (f"After the discount, the total comes to ${self.total}.")
+    else:
+      print('There is no discount to apply.')
+  
+  def void_last_transaction(self):
+    self.total -= self.prev_total
